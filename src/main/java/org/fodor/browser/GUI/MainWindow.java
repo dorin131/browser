e4476a3fc8d39d0fc4bbc015ae67981af784398d
Dorin <dorin.fodor@gmail.com>
2020-05-07 21:47:39 +0100

    [GUI] Added Console pane

--- src/main/java/org/fodor/browser/GUI/MainWindow.java
+++ src/main/java/org/fodor/browser/GUI/MainWindow.java
@@ -12,20 +12,28 @@ public class MainWindow {
     private JPanel topPanel;
     private JPanel bottomPanel;
     private JLabel statusLabel;
+    private JPanel middlePanel;
+    private JTextArea consoleTextArea;
+    private JPanel consolePanel;
+    private JSplitPane splitPane;
+    private JTextField textField1;
 
     public MainWindow() {
-       this.mainFrame = new JFrame("Browser");
+        this.mainFrame = new JFrame("Browser");
         goButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(null, "hello");
             }
         });
+
+        this.splitPane.setResizeWeight(1);
     }
 
     public void show() {
         this.mainFrame.setContentPane(new MainWindow().mainPanel);
         this.mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.mainFrame.pack();
         this.mainFrame.setVisible(true);
     }
