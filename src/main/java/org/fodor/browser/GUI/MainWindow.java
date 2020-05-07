23c50a019b1e25fac733bfdf5da5323ba7c9eaf9
Dorin <dorin.fodor@gmail.com>
2020-05-07 21:12:49 +0100

    [GUI] Use the GUI builder to create GUI

--- src/main/java/org/fodor/browser/GUI/MainWindow.java
+++ src/main/java/org/fodor/browser/GUI/MainWindow.java
@@ -1,35 +1,32 @@
 package org.fodor.browser.GUI;
 
-import org.fodor.browser.GUI.components.Canvas;
-import org.fodor.browser.GUI.components.Top;
-
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainWindow {
-    JFrame frame;
-    Top top;
-    Canvas canvas;
+    private JFrame mainFrame;
+    private JPanel mainPanel;
+    private JTextField addressField;
+    private JButton goButton;
+    private JPanel topPanel;
+    private JPanel bottomPanel;
+    private JLabel statusLabel;
 
     public MainWindow() {
-        frame = new JFrame();
-        top = new Top();
-        canvas = new Canvas();
-
-        frame.setTitle("Browser");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.getContentPane().add(top);
-        frame.getContentPane().add(canvas);
-
-        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
-        frame.setSize(800, 600);
-        frame.setMinimumSize(new Dimension(250, 250));
+       this.mainFrame = new JFrame("Browser");
+        goButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "hello");
+            }
+        });
     }
 
     public void show() {
-        frame.setVisible(true);
+        this.mainFrame.setContentPane(new MainWindow().mainPanel);
+        this.mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.mainFrame.pack();
+        this.mainFrame.setVisible(true);
     }
 }
