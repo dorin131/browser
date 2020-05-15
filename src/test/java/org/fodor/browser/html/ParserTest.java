74a62b52c3e4f86a3205d2e5920b8d96f957ea20
Dorin <dorin.fodor@gmail.com>
2020-05-15 16:50:18 +0100

    [HTML] Added parser tests

--- src/test/java/org/fodor/browser/html/ParserTest.java
+++ src/test/java/org/fodor/browser/html/ParserTest.java
@@ -28,4 +28,50 @@ class ParserTest {
         assertEquals(ElementType.TEXT, text.getType());
         assertEquals("Hello", ((TextElement) text).getContent());
     }
+
+    @Test
+    void parse2() {
+        String input = "<div>Hello<div>World</div></div>";
+//        Token[] expected = {
+//                new Token(Token.Type.OPEN_TAG, "div"),
+//                new Token(Token.Type.TEXT, "Hello"),
+//                new Token(Token.Type.OPEN_TAG, "div"),
+//                new Token(Token.Type.TEXT, "World"),
+//                new Token(Token.Type.CLOSE_TAG, "div"),
+//                new Token(Token.Type.CLOSE_TAG, "div"),
+//        };
+
+        Tokenizer tokenizer = new Tokenizer(input);
+        DOM dom = new Parser(tokenizer).parse();
+
+        assertEquals(1, dom.getChildren().size());
+        var div = dom.getChildren().get(0);
+        assertEquals(ElementType.DIV, div.getType());
+        assertEquals(2, div.getChildren().size());
+        var text = div.getChildren().get(0);
+        assertEquals(ElementType.TEXT, text.getType());
+        assertEquals("Hello", ((TextElement) text).getContent());
+        var div2 = div.getChildren().get(1);
+        assertEquals(ElementType.DIV, div2.getType());
+        assertEquals(1, div2.getChildren().size());
+        var text2 = div2.getChildren().get(0);
+        assertEquals(ElementType.TEXT, text2.getType());
+        assertEquals("World", ((TextElement) text2).getContent());
+    }
+
+    @Test
+    void parse3() {
+        String input = "Dorin";
+//        Token[] expected = {
+//                new Token(Token.Type.TEXT, "Dorin"),
+//        };
+
+        Tokenizer tokenizer = new Tokenizer(input);
+        DOM dom = new Parser(tokenizer).parse();
+
+        assertEquals(1, dom.getChildren().size());
+        var text = dom.getChildren().get(0);
+        assertEquals(ElementType.TEXT, text.getType());
+        assertEquals("Dorin", ((TextElement) text).getContent());
+    }
 }
